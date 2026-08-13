54eb266441e4aa3c94601cb1572b314cb794f08d
AnsElgyar <anselgyar27@gmail.com>
2026-08-14 00:16:05 +0300

    Adding User Exists Test

--- Programming-Contest-Platform.Tests/Services/UserServiceTests.cs
+++ Programming-Contest-Platform.Tests/Services/UserServiceTests.cs
@@ -1,8 +1,9 @@
-using Microsoft.AspNetCore.Identity;
+using System.Runtime.InteropServices.Marshalling;
+using Azure.Core.Pipeline;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
 using Moq;
 using Programming_Contest_Platform.Data;
-using Programming_Contest_Platform.DTO;
 using Programming_Contest_Platform.Entity;
 using Programming_Contest_Platform.Services;
 public class UserTests
@@ -27,14 +28,40 @@ public class UserTests
     [Fact]
     public async Task GetUserProfileAsync_WhenUserIdIsNull_ShouldReturnNull()
     {
-        // Arrange 
-        Guid? userId = null;
-
-        // Act 
+        Guid? userId = null;         
         var result = await _sut.GetUserProfileAsync(userId ?? Guid.Empty);
-
-        // Assert 
+        
         Assert.Null(result);
+    }
+
+    [Fact]
+    public async Task GetUserProfileAsync_WhenUserExists_ShouldReturnUser()
+    {
+        Guid UserId = Guid.NewGuid();
+        string? FullName = "Ans Elgyar";
+        string PasswordHash = "";
+        string Role = "User";
+        string encryptedName = "ABCD";
+        string decryptedName = "Ans Taher";
+        var user = new User
+        {
+            Id = UserId,
+            FullName = FullName,
+            Username = encryptedName,
+            PasswordHash = PasswordHash, 
+            Role = Role
+        };
+
+        _encryptionServiceMock
+                .Setup(repo => repo.Encrypt(encryptedName))
+                .Returns(decryptedName);
+
+        user.Username = decryptedName;
+
+        
+        Assert.Equal(user.Username, encryptedName);
+        Assert.Equal(user.Username, encryptedName);
+
     }
     
 }
